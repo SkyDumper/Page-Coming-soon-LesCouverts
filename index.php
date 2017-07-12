e54a373252cae58de4c29013bb50238b441934e0
Victor <contact.victor.gousset@gmail.com>
2017-07-12 19:36:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victorgousset
+ * Date: 12/07/2017
+ * Time: 18:18
+ */
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr" xml:lang="fr">
+<head>
+    <meta content="text/html" charset="utf-8" />
+    <title>Network LesCouverts</title>
+
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <div class="drapeau">
+        <a href="#"><img src="img/France.png" width="60px;"/></a>
+        <a href="en"><img src="img/En.png" width="60px;"/></a>
+    </div>
+</header>
+
+<div class="center">
+    <div class="moove">
+        <img src="img/logo.png" width="256px;" />
+    </div>
+
+    <h1>Network LesCouverts</h1>
+
+    <br>
+    <p>Après avoir accueilli plus de 15 000 joueurs sur notre première version.</p>
+    <p style="position: relative; bottom: 20px;">Nous avons décidé de faire évoluer le Network en repartant de 0..</p>
+    <p style="position: relative; bottom: 40px;">Nous revenons très prochainement et nous vous remercions pour votre soutien.</p>
+
+    <br><br>
+    <a href="https://discord.gg/H63Uvb"><img src="img/discord.png" width="250px;" /></a>
+</div>
+
+
+
+
+</body>
+</html>
